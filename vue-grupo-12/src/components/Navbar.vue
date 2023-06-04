2b695ea14625e9e185f90e23146c9173a7991459
Camila González <ccgonzalez3@uc.cl>
2023-06-04 13:04:56 -0400

    Add Pinterest files

---
+++ vue-grupo-12/src/components/Navbar.vue
@@ -0,0 +1,167 @@
+<script setup>
+</script>
+
+<template>
+  <div class="pinterest">
+    <div class="left">
+      <a href="#" class="logo">
+        <img src="icons-red/manifest-icon-192.maskable.png" alt="">
+      </a>
+    </div>
+    <div class="search">
+      <i class="fas fa-search"></i>
+      <input type="search" name="" placeholder="Search" id="">
+    </div>
+    <div class="right">
+      <a href="#" class="items"><i class="fas fa-bell"></i></a>
+      <a href="#" class="items"><i class="far fa-comment-dots"></i></a>
+      <a href="#" class="avatar">
+        <div class="img"><img src="https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80" alt=""></div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 1rem;
+    font-family: 'Montserrat', sans-serif;
+  }
+  body {
+    background: #f6f6f6;
+  }
+  * a {
+    text-decoration: none;
+    color: #151515;
+  }
+  .pinterest {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #fff;
+    padding: 0.938rem;
+  }
+  .left {
+    display: flex;
+    align-items: center;
+    width: 20%;
+  }
+  .left .logo {
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .left .logo:hover {
+    background-color: #e5e5e5;
+  }
+  .left .logo img {
+    color: #e60022;
+    font-size: 24px;
+    height: inherit;
+    width: inherit;
+  }
+  .left .home {
+    font-weight: bold;
+    height: 3rem;
+    color: #fff;
+    padding: 1rem;
+    background-color: #151515;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1.563rem;
+  }
+  .avatar {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .avatar .img {
+    height: 2rem;
+    width: 2rem;
+    position: relative;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+  .avatar .img img {
+    position: absolute;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+  .search {
+    width: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 3rem;
+    border-radius: 25px;
+    overflow: hidden;
+    background: #e3e3e3;
+  }
+  .search:hover {
+    background: #c9c9c9;
+  }
+  .search i {
+    width: 3rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #767676;
+  }
+  .search input {
+    width: 100%;
+    border: none;
+    background: none;
+    outline: none;
+    padding-right: 1rem;
+  }
+  .search input::placeholder {
+    color: #767676;
+    font-size: 1rem;
+  }
+  .right {
+    width: 20%;
+    display: flex;
+    align-items: center;
+  }
+  .items {
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .items:hover {
+    background: #e5e5e5;
+  }
+  .items i {
+    font-size: 1rem;
+    color: #767676;
+  }
+  .items-down {
+    border-radius: 50%;
+    width: 1rem;
+    height: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.7rem;
+  }
+  .items-down:hover {
+    background: #e5e5e5;
+  }
+</style>
---
+++ vue-grupo-12/src/components/Pin.vue
@@ -0,0 +1,49 @@
+<script setup>
+  defineProps({
+    id: Number,
+    url: String,
+    description: String,
+    size: String,
+    likes: Number
+  })
+</script>
+
+<template>
+  <div :class="`card ${size}`" >
+    <img :src=url />
+  </div>
+</template>
+
+<style scoped>
+  :root {
+    --card_border_radius: 16px;
+    --card_small: 26;
+    --card_medium: 33;
+    --card_large: 45;
+  }
+  .card {
+    padding: 0;
+    margin: 15px 10px;
+    border-radius: var(--card_border_radius);
+    background-color: grey;
+  }
+
+  .card_small {
+    grid-row-end: span var(--card_small);
+  }
+
+  .card_medium {
+    grid-row-end: span var(--card_medium);
+  }
+
+  .card_large {
+    grid-row-end: span var(--card_large);
+  }
+
+  .card img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: var(--card_border_radius);
+  }
+</style>
